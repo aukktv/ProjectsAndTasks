47011485503ac52d1336380185c228d1e3fb92c0
Auksė Kotovičienė <aukse.kotoviciene@gmail.com>
2020-08-01 13:05:24 +0300

    Custom exception for unique projectidentifiere

--- projectsAndTasksbBackend/src/main/java/lt/aukktv/projectsAndTasksbBackend/services/ProjectService.java
+++ projectsAndTasksbBackend/src/main/java/lt/aukktv/projectsAndTasksbBackend/services/ProjectService.java
@@ -4,17 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lt.aukktv.projectsAndTasksbBackend.domain.Project;
+import lt.aukktv.projectsAndTasksbBackend.exceptions.ProjectIdException;
 import lt.aukktv.projectsAndTasksbBackend.repositories.ProjectRepository;
 
 @Service
 public class ProjectService {
-	
+
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
 	public Project saveOrUpdateProject(Project project) {
-		
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException(
+					"Project identifier " + project.getProjectIdentifier().toUpperCase() + " allredy exist");
+		}
+
 	}
 
 }
