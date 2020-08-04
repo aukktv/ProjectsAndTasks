8a4b6d5cf2c7fcd50485b473e2da25962c95bda2
Auksė Kotovičienė <aukse.kotoviciene@gmail.com>
2020-08-04 15:50:55 +0300

    Created firs components: Layout, Project, Dashboard

---
+++ projectsandtasksfrontend/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ProjectItem />
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
