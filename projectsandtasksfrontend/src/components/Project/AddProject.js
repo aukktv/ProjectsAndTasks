2b54ae6b452012789fc3ccb5569f24407bbb49ba
Auksė Kotovičienė <aukse.kotoviciene@gmail.com>
2020-08-05 12:46:47 +0300

    AddProject form funcyionality added

--- projectsandtasksfrontend/src/components/Project/AddProject.js
+++ projectsandtasksfrontend/src/components/Project/AddProject.js
@@ -11,7 +11,26 @@ class AddProject extends Component {
             "description": "",
             "start_date": "",
             "end_date": ""
-        }
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const newProject = {
+            "projectName": this.state.projectName,
+            "projectIdentifier": this.state.projectIdentifier,
+            "description": this.state.description,
+            "start_date": this.state.start_date,
+            "end_date": this.state.end_date
+        };
+        console.log(newProject);
     }
 
     render() {
@@ -22,7 +41,7 @@ class AddProject extends Component {
                         <div className="col-md-8 m-auto">
                             <h5 className="display-4 text-center">Create Project form</h5>
                             <hr />
-                            <form>
+                            <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
                                     <input
                                         type="text"
@@ -30,6 +49,7 @@ class AddProject extends Component {
                                         placeholder="Project Name"
                                         name="projectName"
                                         value={this.state.projectName}
+                                        onChange={this.onChange}
                                     />
                                 </div>
                                 <div className="form-group">
@@ -39,6 +59,7 @@ class AddProject extends Component {
                                         placeholder="Unique Project ID"
                                         name="projectIdentifier"
                                         value={this.state.projectIdentifier}
+                                        onChange={this.onChange}
                                     />
                                 </div>
 
@@ -48,6 +69,7 @@ class AddProject extends Component {
                                         placeholder="Project Description"
                                         name="description"
                                         value={this.state.description}
+                                        onChange={this.onChange}
                                     />
                                 </div>
                                 <h6>Start Date</h6>
@@ -57,6 +79,7 @@ class AddProject extends Component {
                                         className="form-control form-control-lg"
                                         name="start_date"
                                         value={this.state.start_date}
+                                        onChange={this.onChange}
                                     />
                                 </div>
                                 <h6>Estimated End Date</h6>
@@ -66,6 +89,7 @@ class AddProject extends Component {
                                         className="form-control form-control-lg"
                                         name="end_date"
                                         value={this.state.end_date}
+                                        onChange={this.onChange}
                                     />
                                 </div>
 
