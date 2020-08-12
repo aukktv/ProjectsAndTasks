59249593d96b904c88684cf7f90a324b055da643
Auksė Kotovičienė <aukse.kotoviciene@gmail.com>
2020-08-12 20:53:04 +0300

    Update project task - part first - data sended to state

--- projectsandtasksfrontend/src/App.js
+++ projectsandtasksfrontend/src/App.js
@@ -10,6 +10,7 @@ import store from "./store";
 import UpdateProject from './components/Project/UpdateProject';
 import ProjectBoard from './components/ProjectBoard/ProjectBoard';
 import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask';
+import UpdateProjectTask from './components/ProjectBoard/ProjectTasks/UpdateProjectTask';
 
 
 function App() {
@@ -22,8 +23,8 @@ function App() {
           <Route exact path="/addProject" component={AddProject} />
           <Route exact path="/updateProject/:id" component={UpdateProject} />
           <Route exact path="/projectBoard/:id" component={ProjectBoard} />
-
           <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
+          <Route exact path="/updateProjectTask/:backlog_id/:pt_id" component={UpdateProjectTask} />
         </div>
       </Router>
     </Provider>
--- projectsandtasksfrontend/src/components/ProjectBoard/ProjectBoard.js
+++ projectsandtasksfrontend/src/components/ProjectBoard/ProjectBoard.js
@@ -8,14 +8,53 @@ import { getBacklog } from "../../actions/backlogAction";
 
 class ProjectBoard extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            errors: {}
+        }
+    }
+
     componentDidMount() {
         const { id } = this.props.match.params;
         this.props.getBacklog(id);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+    }
+
     render() {
         const { id } = this.props.match.params;
-        const { project_tasks } = this.props.backlog
+        const { project_tasks } = this.props.backlog;
+        const { errors } = this.state;
+
+        let boardContent;
+
+        const boardAlgorithm = (errors, project_tasks) => {
+            if (project_tasks.length < 1) {
+                if (errors.projectNotFoundMessage) {
+                    return (
+                        <div className="alert alert-danger text-center" role="alert">
+                            {errors.projectNotFoundMessage}
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div className="alert alert-info text-center" role="alert">
+                            No project tasks on this board
+                        </div>
+                    );
+                }
+            } else {
+                return <Backlog project_tasks_props={project_tasks} />
+            }
+        };
+
+        boardContent = boardAlgorithm(errors, project_tasks);
+
         return (
             <div className="container">
                 <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -23,7 +62,7 @@ class ProjectBoard extends Component {
                 </Link>
                 <br />
                 <hr />
-                <Backlog project_tasks_props={project_tasks} />
+                {boardContent}
             </div>
         );
     }
@@ -31,11 +70,13 @@ class ProjectBoard extends Component {
 
 ProjectBoard.propTypes = {
     backlog: PropTypes.object.isRequired,
-    getBacklog: PropTypes.func.isRequired
-}
+    getBacklog: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-    backlog: state.backlog
+    backlog: state.backlog,
+    errors: state.errors
 })
 
 export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
