54a616b357daf8d9ec2dbc72b5e97c3a325c5f56
Auksė Kotovičienė <aukse.kotoviciene@gmail.com>
2020-08-13 13:11:05 +0300

    Delete project task

--- projectsandtasksfrontend/src/reducers/backlogReducer.js
+++ projectsandtasksfrontend/src/reducers/backlogReducer.js
@@ -22,9 +22,9 @@ export default function (state = initialState, action) {
 
         case DELETE_PROJECT_TASK:
             return {
-                ...state
-                //filter
-            }
+                ...state,
+                project_tasks: state.project_tasks.filter(project_task => project_task.projectSequence !== action.payload)
+            };
 
         default:
             return state;
